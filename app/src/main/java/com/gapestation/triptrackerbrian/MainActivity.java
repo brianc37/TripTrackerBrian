2694beadbe302b3211069f7ee8bb44e197df0c4d
bchamberlain <brianusa2001@gmail.com>
2019-04-24 10:35:04 -0400

    jawn

--- app/src/main/java/com/gapestation/triptrackerbrian/MainActivity.java
+++ app/src/main/java/com/gapestation/triptrackerbrian/MainActivity.java
@@ -1,11 +1,18 @@
 package com.gapestation.triptrackerbrian;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.backendless.Backendless;
+import com.backendless.BackendlessUser;
+import com.backendless.async.callback.AsyncCallback;
+import com.backendless.exceptions.BackendlessFault;
 
 public class MainActivity extends AppCompatActivity {
     EditText mEmailEditText;
@@ -20,12 +27,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Backendless.initApp( this,
+                getString(R.string.APP_ID),
+                getString(R.string.API_KEY));
+
         mEmailEditText = (EditText)findViewById(R.id.enter_email);
         mPasswordEditText = (EditText)findViewById(R.id.enter_password);
         mNameEditText = (EditText)findViewById(R.id.enter_name);
         mLoginButton = (Button)findViewById(R.id.login_button);
         mSignUpTextView = (TextView)findViewById(R.id.sign_up_text);
         mSignUpButton = (Button)findViewById(R.id.sign_up_button);
+        final BackendlessUser user = new BackendlessUser();
+        final String TAG = MainActivity.class.getName();
 
         mSignUpTextView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,6 +54,47 @@ public class MainActivity extends AppCompatActivity {
                     mSignUpButton.setVisibility(View.GONE);
                     mLoginButton.setVisibility(View.VISIBLE);
                     mSignUpTextView.setText(R.string.sign_up_text);
+                }
+            }
+        });
+
+        mSignUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userEmail = mEmailEditText.getText().toString();
+                String password = mPasswordEditText.getText().toString();
+                String name = mNameEditText.getText().toString();
+
+                userEmail = userEmail.trim();
+                password = password.trim();
+                name = name.trim();
+
+                if (!userEmail.isEmpty() &&!password.isEmpty() && !name.isEmpty()) {
+                    user.setEmail(userEmail);
+                    user.setPassword(password);
+                    user.setProperty("name", name);
+
+                    Backendless.UserService.register(user,
+                            new AsyncCallback<BackendlessUser>() {
+                                @Override
+                                public void handleResponse( BackendlessUser backendlessUser ) {
+                                    Log.i(TAG, "Registration successful for " +
+                                            backendlessUser.getEmail());
+                                }
+                                @Override
+                                public void handleFault( BackendlessFault fault ) {
+                                    Log.i(TAG, "Registration failed: " + fault.getMessage());
+                                }
+                            } );
+
+                }
+                else {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    builder.setMessage(R.string.empty_field_signup_error);
+                    builder.setTitle(R.string.authentication_error_title);
+                    builder.setPositiveButton(android.R.string.ok, null);
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
                 }
             }
         });
